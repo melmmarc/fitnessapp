553ffab7fc2184e017082b3ecb4a122026c81d49
Marc Vogel <inf3462@hs-worms.de>
2023-12-30 01:17:04 +0100

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  selectedGender: string = ''; // Initial value
+  selectedUsername: string = ''; // Initial value
+  selectedGoal: string = ''; // Initial value
 
+  selectGender(gender: string) {
+    this.selectedGender = gender;
+    console.log('Selected gender: ', this.selectedGender);
+  }
+
+  updateUsername(username: string) {
+    this.selectedUsername = username;
+    console.log('Selected username: ', this.selectedUsername);
+  }
+
+  selectGoal(goal: string) {
+    this.selectedGoal = goal;
+    console.log('Selected goal: ', this.selectedGoal);
+  }
 }
