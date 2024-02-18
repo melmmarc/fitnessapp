f0fac34f2281a6d6858450f376a92dbb1a85c39d
Marc Vogel <inf3462@hs-worms.de>
2024-02-18 21:55:51 +0100

    added stopwatch

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -8,7 +8,42 @@ import { Router } from '@angular/router';
 })
 export class TimerComponent {
 
+  time: number = 0;
+  timerRef: any;
+  isRunning: boolean = false;
+
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.startTimer();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
+  }
+
+  startTimer() {
+    this.isRunning = true;
+    this.timerRef = setInterval(() => {
+      this.time++;
+    }, 1000);
+  }
+
+  stopTimer() {
+    this.isRunning = false;
+    clearInterval(this.timerRef);
+  }
+
+  resetTimer() {
+    this.time = 0;
+    clearInterval(this.timerRef);
+  }
+
+  formatTime() {
+    const minutes: string = Math.floor(this.time / 60).toString().padStart(2, '0');
+    const seconds: string = (this.time % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  }
 
   goToMenu(){
     this.router.navigateByUrl('/menu');
