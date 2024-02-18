44bf6b9fd5373e2f0dd10da81ffffede783b4d21
Marc Vogel <inf3462@hs-worms.de>
2024-02-18 21:33:33 +0100

    added bmi rechner

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -8,6 +8,10 @@ import { Router } from '@angular/router';
 })
 export class BmiComponent {
 
+  height:number = 0;
+  weight:number = 0;
+  bmi: number= 0;
+
   constructor(private router: Router) {}
 
   goToMenu(){
@@ -24,6 +28,26 @@ export class BmiComponent {
 
   goToTimer(){
     this.router.navigateByUrl('/timer');
+  }
+
+  calculateBMI() {
+    if (this.height && this.weight) {
+      this.bmi = this.weight / ((this.height / 100) ** 2);
+    }
+  }
+
+  getBMICategory(bmi: number): string {
+    if (bmi < 16) {
+      return 'Kritisches Untergewicht';
+    } else if (bmi >= 16 && bmi < 18.5) {
+      return 'Untergewicht';
+    } else if (bmi >= 18.5 && bmi < 25) {
+      return 'Normalgewicht';
+    } else if (bmi >= 25 && bmi < 30) {
+      return 'Leichtes Übergewicht';
+    } else {
+      return 'Übergewicht';
+    }
   }
 
 }
