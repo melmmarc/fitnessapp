33d42c00ec02500b4bc06a7adf3f1c202dda8625
Marc Vogel <inf3462@hs-worms.de>
2024-02-19 01:11:54 +0100

    fixed avatars

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,6 +13,14 @@ export class HomeComponent {
   selectedUsername: string = ''; 
   selectedGoal: string = ''; 
 
+  female2Selected:boolean = false;
+  female3Selected:boolean = false;
+  female5Selected:boolean = false;
+
+  male1Selected:boolean = false;
+  male4Selected:boolean = false;
+  male5Selected:boolean = false;
+
   updateUsername(username: string) {
     this.selectedUsername = username;
     console.log('Selected username: ', this.selectedUsername);
@@ -25,5 +33,83 @@ export class HomeComponent {
 
   goToMenu() {
     this.router.navigateByUrl('/menu');
+  }
+
+  toggleFemale2():void{
+    if(this.female2Selected == false)
+    {
+      this.female2Selected = true;
+      this.female3Selected = false;
+      this.female5Selected = false;
+
+      this.male1Selected = false;
+      this.male4Selected = false;
+      this.male5Selected = false;
+    }
+  }
+
+  toggleFemale3():void{
+    if(this.female3Selected == false)
+    {
+      this.female2Selected = false;
+      this.female3Selected = true;
+      this.female5Selected = false;
+
+      this.male1Selected = false;
+      this.male4Selected = false;
+      this.male5Selected = false;
+    }
+  }
+
+  toggleFemale5():void{
+    if(this.female5Selected == false)
+    {
+      this.female2Selected = false;
+      this.female3Selected = false;
+      this.female5Selected = true;
+
+      this.male1Selected = false;
+      this.male4Selected = false;
+      this.male5Selected = false;
+    }
+  }
+
+  toggleMale1():void{
+    if(this.male1Selected == false)
+    {
+      this.female2Selected = false;
+      this.female3Selected = false;
+      this.female5Selected = false;
+
+      this.male1Selected = true;
+      this.male4Selected = false;
+      this.male5Selected = false;
+    }
+  }
+
+  toggleMale4():void{
+    if(this.male4Selected == false)
+    {
+      this.female2Selected = false;
+      this.female3Selected = false;
+      this.female5Selected = false;
+
+      this.male1Selected = false;
+      this.male4Selected = true;
+      this.male5Selected = false;
+    }
+  }
+
+  toggleMale5():void{
+    if(this.male5Selected == false)
+    {
+      this.female2Selected = false;
+      this.female3Selected = false;
+      this.female5Selected = false;
+
+      this.male1Selected = false;
+      this.male4Selected = false;
+      this.male5Selected = true;
+    }
   }
 }
