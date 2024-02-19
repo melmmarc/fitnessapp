ee7f15f1c118e05afd841ac87120aa7411a44e2e
Marc Vogel <inf3462@hs-worms.de>
2024-02-19 19:19:54 +0100

    added calorie calculator

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -12,6 +12,11 @@ export class BmiComponent {
   weight:number = 80;
   bmi: number= 0;
 
+  age: number = 30;
+  gender: string = 'male';
+  activityLevel: number = 1.55; // Default activity level
+  dailyCalorieIntake: number = 0;
+
   constructor(private router: Router) {}
 
   goToMenu(){
@@ -51,6 +56,20 @@ export class BmiComponent {
       return 'Leichtes Übergewicht';
     } else {
       return 'Übergewicht';
+    }
+  }
+
+  calculateDailyCalorieIntake() {
+    // Implement your chosen formula to calculate daily calorie intake
+    // For example, using the Mifflin-St Jeor Equation
+    if (this.age && this.gender && this.weight && this.height && this.activityLevel) {
+      let bmr: number;
+      if (this.gender === 'male') {
+        bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+      } else {
+        bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
+      }
+      this.dailyCalorieIntake = bmr * this.activityLevel;
     }
   }
 
