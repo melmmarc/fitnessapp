dc8f8f190ed54f89fa71bd6d41bfa039c51c9386
Marc Vogel <inf3462@hs-worms.de>
2024-02-19 16:45:39 +0100

    added buttons

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -8,10 +8,6 @@ import { Router } from '@angular/router';
 })
 export class SettingsComponent {
 
-  height:number = 180;
-  weight:number = 80;
-  bmi: number= 0;
-
   constructor(private router: Router) {}
 
   goToMenu(){
@@ -28,26 +24,6 @@ export class SettingsComponent {
 
   goToTimer(){
     this.router.navigateByUrl('/timer');
-  }
-
-  calculateBMI() {
-    if (this.height && this.weight) {
-      this.bmi = this.weight / ((this.height / 100) ** 2);
-    }
-  }
-
-  getBMICategory(bmi: number): string {
-    if (bmi < 16) {
-      return 'Kritisches Untergewicht';
-    } else if (bmi >= 16 && bmi < 18.5) {
-      return 'Untergewicht';
-    } else if (bmi >= 18.5 && bmi < 25) {
-      return 'Normalgewicht';
-    } else if (bmi >= 25 && bmi < 30) {
-      return 'Leichtes Übergewicht';
-    } else {
-      return 'Übergewicht';
-    }
   }
 
 }
